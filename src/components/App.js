60c3ca232bea35616683637d76657a4f25b3462a
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-04-08 19:23:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import '../styles/App.css';
 
 class Odd extends React.Component {
@@ -25,6 +25,59 @@ class Even extends React.Component {
 class App extends React.Component {
   handleChange(){
     this.setState({even: !this.state.even})
+  }
+  constructor(props){
+    super(props)
+    this.state = {even: true}
+    this.handleChange =  this.handleChange.bind(this)
+  }
+  render() {
+    return (
+      <div id="main">
+        {this.state.even ? <Even /> : <Odd />}
+
+        <button id="toggle" onClick={this.handleChange}>Change</button>
+      </div>
+    )
+  }
+
+}
+
+
+export default App;*/
+
+
+
+import React from 'react'
+import '../styles/App.css';
+
+class Odd extends React.Component {
+
+  render() {
+//     console.log("Even is unmounted");
+    return (
+      <div id="odd">
+        I am odd
+      </div>
+    )
+  }
+}
+
+class Even extends React.Component {
+
+  render() {
+//     console.log("Odd is unmounted");
+    return (
+      <div id="even">
+        I am even
+      </div>
+    )
+  }
+}
+class App extends React.Component {
+  handleChange(){
+    this.setState({even: !this.state.even});
+    this.state.even? console.log("Even is unmounted"): console.log("Odd is unmounted")
   }
   constructor(props){
     super(props)
